d1a816833df657c76da6dd494b0b9c8eac07cfbd
Fahrul Yurisnan <fahrul4215@gmail.com>
2018-09-24 10:44:28 +0700

    penyatuan database + relationship

--- assets/database/puskesmas.sql
+++ assets/database/puskesmas.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 24, 2018 at 04:47 AM
--- Server version: 10.1.19-MariaDB
--- PHP Version: 5.6.28
+-- Generation Time: Sep 24, 2018 at 05:43 AM
+-- Server version: 10.1.26-MariaDB
+-- PHP Version: 7.1.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -28,7 +30,6 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `detail_resep` (
   `id_detail_resep` int(11) NOT NULL,
-  `fk_resep` int(11) NOT NULL,
   `fk_obat` int(11) NOT NULL,
   `jumlah` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -43,6 +44,31 @@ CREATE TABLE `dokter` (
   `id_dokter` int(11) NOT NULL,
   `nama` varchar(255) NOT NULL,
   `spesialis` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jenis_kartu`
+--
+
+CREATE TABLE `jenis_kartu` (
+  `id_jeniskartu` int(11) NOT NULL,
+  `nama_kartu` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `obat`
+--
+
+CREATE TABLE `obat` (
+  `id_obat` int(11) NOT NULL,
+  `exp_date` date NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `jenis_obat` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -76,11 +102,52 @@ CREATE TABLE `penyakit` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `petugas`
+--
+
+CREATE TABLE `petugas` (
+  `id_petugas` int(11) NOT NULL,
+  `fk_poli` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `poli`
+--
+
+CREATE TABLE `poli` (
+  `id_poli` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `keterangan` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rekam_medis`
+--
+
+CREATE TABLE `rekam_medis` (
+  `id_rekam_medis` int(11) NOT NULL,
+  `fk_pasien` int(11) NOT NULL,
+  `fk_penyakit` int(11) NOT NULL,
+  `fk_dokter` int(11) NOT NULL,
+  `fk_resep` int(11) NOT NULL,
+  `diagnosa` varchar(100) NOT NULL,
+  `tanggal` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `resep`
 --
 
 CREATE TABLE `resep` (
   `id_resep` int(11) NOT NULL,
+  `fk_detail_resep` int(11) NOT NULL,
   `tanggal` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -93,7 +160,6 @@ CREATE TABLE `resep` (
 --
 ALTER TABLE `detail_resep`
   ADD PRIMARY KEY (`id_detail_resep`),
-  ADD KEY `fk_resep` (`fk_resep`),
   ADD KEY `fk_obat` (`fk_obat`);
 
 --
@@ -101,6 +167,18 @@ ALTER TABLE `detail_resep`
 --
 ALTER TABLE `dokter`
   ADD PRIMARY KEY (`id_dokter`);
+
+--
+-- Indexes for table `jenis_kartu`
+--
+ALTER TABLE `jenis_kartu`
+  ADD PRIMARY KEY (`id_jeniskartu`);
+
+--
+-- Indexes for table `obat`
+--
+ALTER TABLE `obat`
+  ADD PRIMARY KEY (`id_obat`);
 
 --
 -- Indexes for table `pasien`
@@ -117,25 +195,54 @@ ALTER TABLE `penyakit`
   ADD KEY `fk_poli` (`fk_poli`);
 
 --
+-- Indexes for table `petugas`
+--
+ALTER TABLE `petugas`
+  ADD PRIMARY KEY (`id_petugas`),
+  ADD KEY `fk_poli` (`fk_poli`);
+
+--
+-- Indexes for table `poli`
+--
+ALTER TABLE `poli`
+  ADD PRIMARY KEY (`id_poli`);
+
+--
+-- Indexes for table `rekam_medis`
+--
+ALTER TABLE `rekam_medis`
+  ADD PRIMARY KEY (`id_rekam_medis`),
+  ADD KEY `fk_pasien` (`fk_pasien`,`fk_penyakit`,`fk_dokter`,`fk_resep`),
+  ADD KEY `fk_dokter` (`fk_dokter`),
+  ADD KEY `fk_resep` (`fk_resep`),
+  ADD KEY `fk_penyakit` (`fk_penyakit`);
+
+--
 -- Indexes for table `resep`
 --
 ALTER TABLE `resep`
-  ADD PRIMARY KEY (`id_resep`);
+  ADD PRIMARY KEY (`id_resep`),
+  ADD KEY `fk_detail_resep` (`fk_detail_resep`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `detail_resep`
---
-ALTER TABLE `detail_resep`
-  MODIFY `id_detail_resep` int(11) NOT NULL AUTO_INCREMENT;
---
 -- AUTO_INCREMENT for table `dokter`
 --
 ALTER TABLE `dokter`
   MODIFY `id_dokter` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `jenis_kartu`
+--
+ALTER TABLE `jenis_kartu`
+  MODIFY `id_jeniskartu` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `obat`
+--
+ALTER TABLE `obat`
+  MODIFY `id_obat` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `pasien`
 --
@@ -147,10 +254,69 @@ ALTER TABLE `pasien`
 ALTER TABLE `penyakit`
   MODIFY `id_penyakit` int(11) NOT NULL AUTO_INCREMENT;
 --
+-- AUTO_INCREMENT for table `petugas`
+--
+ALTER TABLE `petugas`
+  MODIFY `id_petugas` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `poli`
+--
+ALTER TABLE `poli`
+  MODIFY `id_poli` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `rekam_medis`
+--
+ALTER TABLE `rekam_medis`
+  MODIFY `id_rekam_medis` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `resep`
 --
 ALTER TABLE `resep`
   MODIFY `id_resep` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `detail_resep`
+--
+ALTER TABLE `detail_resep`
+  ADD CONSTRAINT `detail_resep_ibfk_1` FOREIGN KEY (`fk_obat`) REFERENCES `obat` (`id_obat`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `pasien`
+--
+ALTER TABLE `pasien`
+  ADD CONSTRAINT `pasien_ibfk_1` FOREIGN KEY (`fk_jenis_kartu`) REFERENCES `jenis_kartu` (`id_jeniskartu`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `penyakit`
+--
+ALTER TABLE `penyakit`
+  ADD CONSTRAINT `penyakit_ibfk_1` FOREIGN KEY (`fk_poli`) REFERENCES `poli` (`id_poli`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `petugas`
+--
+ALTER TABLE `petugas`
+  ADD CONSTRAINT `petugas_ibfk_1` FOREIGN KEY (`fk_poli`) REFERENCES `poli` (`id_poli`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `rekam_medis`
+--
+ALTER TABLE `rekam_medis`
+  ADD CONSTRAINT `rekam_medis_ibfk_1` FOREIGN KEY (`fk_dokter`) REFERENCES `dokter` (`id_dokter`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `rekam_medis_ibfk_2` FOREIGN KEY (`fk_pasien`) REFERENCES `pasien` (`id_pasien`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `rekam_medis_ibfk_3` FOREIGN KEY (`fk_resep`) REFERENCES `resep` (`id_resep`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `rekam_medis_ibfk_4` FOREIGN KEY (`fk_penyakit`) REFERENCES `penyakit` (`id_penyakit`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `resep`
+--
+ALTER TABLE `resep`
+  ADD CONSTRAINT `resep_ibfk_1` FOREIGN KEY (`fk_detail_resep`) REFERENCES `detail_resep` (`id_detail_resep`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
